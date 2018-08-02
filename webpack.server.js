2c9ed6eeaad148c43049665a733206240b0a6b18
Haukur Már Böðvarsson <haukurmar@gmail.com>
2018-08-02 21:56:50 +0000

    Tell webpack to not bundle any libraries into to output bundle if the library exists in node_modules for server

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base')
+const webpackNodeExternals = require('webpack-node-externals')
 
 const config = {
 	// Inform webpack that we're building a bundle for nodeJS, rather than the browser
@@ -12,7 +13,10 @@ const config = {
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'build')
-	}
+	},
+
+	// Tell webpack to not bundle any libraries into to output bundle if the library exists in node_modules
+	externals: [webpackNodeExternals()]
 }
 
 module.exports = merge(baseConfig, config)
