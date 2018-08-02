bfb19399e777ce33b93c51eba620dfcd7262c274
Haukur Már Böðvarsson <haukurmar@gmail.com>
2018-08-02 21:43:54 +0000

    Add base webpack config and merge client and server webpack files to use the base.

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base')
 
-module.exports = {
+const config = {
 	// Inform webpack that we're building a bundle for nodeJS, rather than the browser
 	target: 'node',
 
@@ -10,23 +12,7 @@ module.exports = {
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'build')
-	},
-
-	// Tell webpack to run babel on every file that it runs through
-	module: {
-		rules: [
-			{
-				test: /\.js?$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				options: {
-					presets: [
-						'react',
-						'stage-0',
-						['env', {targets : {browsers: ['last 2 versions']}}]
-					]
-				}
-			}
-		]
 	}
 }
+
+module.exports = merge(baseConfig, config)
